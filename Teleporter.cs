935484eb3032f8edf730c8a6fbf9b7b3cd32fe18
RtZikky <45165736+RtZikky@users.noreply.github.com>
2018-11-19 10:55:04 +0100

    Add files via upload

---
+++ Teleporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+	public GameObject destination;
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name =="CompleteTank(Clone)" || collider.gameObject.name =="CompleteTank(Clone)" && destination != null){
+
+		
+			//foreach(Teleporteur tp in FindObjectsOfType<Teleporteur>()){
+				
+				//if(tp.destination==destination && tp != this){
+					Vector3 position=destination.transform.position;
+					position.y+=1.2f;
+					collider.gameObject.transform.position=position;
+				//}
+			//}
+
+
+		}
+	}	
+}
