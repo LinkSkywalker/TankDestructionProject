2dcd91200de1bd651e34e9e6895315c85cc06fff
RtZikky <45165736+RtZikky@users.noreply.github.com>
2018-11-19 13:47:17 +0100

    Scripte téléporteur 2

--- Teleporter.cs
+++ Teleporter.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour {
 	public GameObject destination;
+	public GameObject wasteleported;
+
 
 	void OnTriggerEnter(Collider collider){
-		if (collider.gameObject.name =="CompleteTank(Clone)" || collider.gameObject.name =="CompleteTank(Clone)" && destination != null){
-
-		
-			//foreach(Teleporteur tp in FindObjectsOfType<Teleporteur>()){
-				
-				//if(tp.destination==destination && tp != this){
-					Vector3 position=destination.transform.position;
-					position.y+=1.2f;
-					collider.gameObject.transform.position=position;
-				//}
-			//}
-
-
+		if (collider.gameObject.name =="CompleteTank(Clone)" || collider.gameObject.name =="CompleteTank(Clone)" && destination != null)
+		{	
+			Complete.TankMovement rb = collider.gameObject.GetComponent<Complete.TankMovement>();
+			if (rb.m_IsInTeleporterZone) {
+				return;
+			}
+			else {
+				Vector3 position=destination.transform.position;
+				//position.y+=1;
+				collider.gameObject.transform.position=position;
+				rb.m_IsInTeleporterZone = true;
+				wasteleported = collider.gameObject;
+			}	
 		}
 	}	
+
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.name =="CompleteTank(Clone)" || collider.gameObject.name =="CompleteTank(Clone)" && destination != null)
+		{	
+			if (wasteleported == collider.gameObject){
+				wasteleported = null;
+				return;
+			}
+			Complete.TankMovement rb = collider.gameObject.GetComponent<Complete.TankMovement>();
+			rb.m_IsInTeleporterZone = false;
+		}
+	}
 }
